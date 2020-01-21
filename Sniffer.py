3304c323a4689019140bed38a7dd4a3f260f2c58
Mnai <Imatlak03@gmail.com>
2020-01-21 18:32:36 -0500

    Formatted to run on Python3, makes successful connection to GET method but URL format is wrong

--- Sniffer.py
+++ Sniffer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 from scapy.all import *
 from datetime import datetime
-import urllib2
+import urllib.request
+import urllib.parse
 import csv
 
 #Holds addresses
@@ -16,31 +17,36 @@ def PacketHandler(pkt):
        # Is packet is a wildcard in wireshark it will not be shown
         if len(pkt.info) > 0:
             # shows packet mac address 
-            testcase = (pkt.addr2 + '---' + pkt.info)
+            testcase = (pkt.addr2, '---', pkt.info)
         else:
-            pkt.info = 'Unknown'
-            testcase = (pkt.addr2 + '---' + pkt.info)
+            pkt.info = "Unknown"
+            testcase = (pkt.addr2, pkt.info)
         if testcase not in clientprobes:
+            # decode pkt.info so b ' is not in the beginning
+            pkt_info = pkt.info.decode("utf-8")
+
             # datetime object containing current date and time
             now = datetime.now()
             time_log = now.strftime('%m/%d/%y %H:%M:%S')
             clientprobes.append(testcase)
-            print ("New Probe Found: " + pkt.addr2 + ' ' +  ' ' + pkt.info + ' ' + time_log)
-            #SendtoServer(pkt.addr2,pkt.info,location,time_log)
+            print ("New Probe Found: ", pkt.addr2, ' ',  ' ', pkt_info, ' ', time_log)
+            SendtoServer(str(pkt.addr2),str(pkt_info),str(location),str(time_log))
             with open('data.csv','a') as file:
                 writer = csv.writer(file)
                 writer.writerow(['MAC','SSID','Time'])
-                writer.writerow([pkt.addr2, pkt.info, time_log])
+                writer.writerow([pkt.addr2, pkt_info, time_log])
 
 def SendtoServer(MAC,SSID,Location,Time):
-    url = urllib2.quote('http://www.ianmatlak.com:8443/add_data.php?MAC='+MAC+'&SSID='+SSID+'&Location='+Location+'&TIME='+Time+'/',':/')
+    url = 'http://www.ianmatlak.com:8443/add_data.php?MAC=' + urllib.parse.quote(MAC + '&SSID='+SSID+'&Location='+Location+'&TIME='+Time +'/') 
+    #+'/',':/')
+    print(str(url))
     # Can be used to replace characters that dont get parsed correctly
     # Might not need to do this though. It still might go through with 
     # %3 and %20 because the system knows those represent white space and ':'
     #url = url.replace('%3',':')
-    wp = urllib2.urlopen(url)
+    wp = urllib.request.urlopen(url)
     pw = wp.read()
     print(pw)
 
-sniff(iface='wlan0mon', prn = PacketHandler,   store=0)
+sniff(iface='wlan0mon', prn = PacketHandler, store=0)
 print('after sniff')
